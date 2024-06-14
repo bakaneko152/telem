6acb0bf4347ecdb6a3118f9847b688ec8cb5add8
bakaneko152 <59172101+bakaneko152@users.noreply.github.com>
2024-06-14 21:54:55 +0900

    Easy Data Select + Fix

--- src/telem/lib/output/GrafanaOutputAdapter.lua
+++ src/telem/lib/output/GrafanaOutputAdapter.lua
@@ -5,14 +5,14 @@ local OutputAdapter     = require 'telem.lib.OutputAdapter'
 local MetricCollection  = require 'telem.lib.MetricCollection'
 
 local component = require("component")
-local internet = require("internet")
+local internet
 
 local GrafanaOutputAdapter = o.class(OutputAdapter)
 GrafanaOutputAdapter.type = 'GrafanaOutputAdapter'
 
 function GrafanaOutputAdapter:constructor (endpoint, apiKey)
     self:super('constructor')
-
+    internet = component.internet
     self.endpoint = assert(endpoint, 'Endpoint is required')
     self.apiKey = assert(apiKey, 'API key is required')
 end
@@ -20,15 +20,77 @@ end
 function GrafanaOutputAdapter:write (collection)
     assert(o.instanceof(collection, MetricCollection), 'Collection must be a MetricCollection')
 
+    local groupmetrics = {}
+    local sep = '^(.-):'
+    for _,v in pairs(collection.metrics) do
+        local adapterreal = (v.adapter and v.adapter ~= '' and '' .. v.adapter) or 'null'
+        local sourcereal = (v.source and v.source ~= '' and '' .. v.source) or 'null'
+        local unitreal = (v.unit and v.unit ~= '' and '' .. v.unit) or 'null'
+
+        local _,_,s = v.name:find(sep)
+        if s == nil then
+            s = 'unknown'
+        end
+        if groupmetrics[s] == nil then
+            groupmetrics[s]={}
+        end
+        local gma = groupmetrics[s]
+        if gma[adapterreal] == nil then
+            gma[adapterreal]={}
+        end
+        local gms = gma[adapterreal]
+        if gms[sourcereal] == nil then
+            gms[sourcereal]={}
+        end
+        local gmu = gms[sourcereal]
+        if gmu[unitreal] == nil then
+            gmu[unitreal]={}
+        end
+        table.insert(gmu[unitreal],v)
+    end
+
     local outf = {}
 
-    for _,v in pairs(collection.metrics) do
-        local unitreal = (v.unit and v.unit ~= '' and ',unit=' .. v.unit) or ''
-        local sourcereal = (v.source and v.source ~= '' and ',source=' .. v.source) or ''
-        local adapterreal = (v.adapter and v.adapter ~= '' and ',adapter=' .. v.adapter) or ''
-
-        table.insert(outf, v.name .. unitreal .. sourcereal .. adapterreal .. (' metric=%f'):format(v.value))
+    sep = '^.-:(.-)$'
+    for prk,adt in pairs(groupmetrics) do
+        local measurementname = prk
+        for adk,srt in pairs(adt) do
+            local adapterreal = (adk ~= 'null' and ',adapter=' .. adk) or ''
+            for srk,unt in pairs(srt) do
+                local sourcereal = (srk ~= 'null' and ',source=' .. srk) or ''
+                for suk,vt in pairs(unt) do
+                    local unitreal = (suk ~= 'null' and ',unit=' .. suk) or ''
+                    local insert_str = measurementname .. unitreal .. sourcereal .. adapterreal
+                    local metricstr = ''
+                    for _,v in pairs(vt) do
+                        local metricname = ''
+                        if measurementname == 'unknown' then
+                            metricname = v.name
+                        elseif v.name ~= nil then
+                            _,_,metricname =  v.name:find(sep)
+                            if metricname == nil then
+                                metricname = 'metric'
+                            end
+                        end
+                        if metricstr=='' then
+                            metricstr = ' ' .. metricname .. ('=%f'):format(v.value)
+                        else
+                            metricstr = metricstr .. ','.. metricname .. ('=%f'):format(v.value)
+                        end
+                    end
+                    table.insert(outf,insert_str .. metricstr)
+                end
+            end
+        end
     end
+
+    -- for _,v in pairs(collection.metrics) do
+    --     local unitreal = (v.unit and v.unit ~= '' and ',unit=' .. v.unit) or ''
+    --     local sourcereal = (v.source and v.source ~= '' and ',source=' .. v.source) or ''
+    --     local adapterreal = (v.adapter and v.adapter ~= '' and ',adapter=' .. v.adapter) or ''
+
+    --     table.insert(outf, v.name .. unitreal .. sourcereal .. adapterreal .. (' metric=%f'):format(v.value))
+    -- end
 
     -- t.pprint(collection)
 
@@ -44,6 +106,7 @@ function GrafanaOutputAdapter:write (collection)
        { Authorization = ('Bearer %s'):format(self.apiKey) }
     )
     res.close()
+    -- print(table.concat(outf, '\n'))
 end
 
 return GrafanaOutputAdapter
